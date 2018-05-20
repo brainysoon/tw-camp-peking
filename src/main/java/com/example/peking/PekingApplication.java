d69c3725f799f5bffda69618df843006c9a99774
brainysoon <brainysoon@gmail.com>
2018-05-20 09:01:04 +0800

    [ Project ] - Init project with spring boot.

---
+++ src/main/java/com/example/peking/PekingApplication.java
@@ -0,0 +1,12 @@
+package com.example.peking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PekingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/peking/PekingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.peking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PekingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
