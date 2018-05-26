a2dcd8517198635660b2d917bd3980330529a78e
brainysoon <brainysoon@gmail.com>
2018-05-26 09:32:24 +0800

    [ Business ] - Add product entity.

---
+++ src/main/java/com/example/peking/entity/Product.java
@@ -0,0 +1,36 @@
+package com.example.peking.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@SelectBeforeUpdate
+@DynamicUpdate
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Integer count;
+
+    private Date modifiedTime;
+
+    private Date createTime;
+
+    private Byte status;
+}
