73c380db74e4dc27899a920e1563044912d6c8b6
brainysoon <brainysoon@gmail.com>
2018-05-29 11:20:11 +0800

    [ Business ] - Add automation test.

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 ./gradlew test
+
+chmod  400 ./stage_icusin_ubuntu
+
+ssh -T -i ./stage_icusin_ubuntu -o StrictHostKeyChecking=no root@stage.icusin.com << stage-icusin-remote
+cd /home
+if [ ! -d "tw-camp-peking" ];then
+    git clone git@github.com:brainysoon/tw-camp-peking.git
+fi
+
+cd tw-camp-peking
+echo running automation test...
+newman run dmallQuiz.postman_collection.json
+stage-icusin-remote
