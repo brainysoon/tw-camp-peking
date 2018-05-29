e54f4d5c5de435e24f69e1bc411951350f22080a
brainysoon <brainysoon@gmail.com>
2018-05-29 09:13:53 +0800

    [ Business ] - Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,17 @@ git pull origin master
 
 echo start building tw-camp-peking
 
-./gradlew bootRun
+./gradlew bootJar
+stage-icusin-remote
+
+
+ssh -T -i ./stage_icusin_ubuntu -o StrictHostKeyChecking=no root@stage.icusin.com << stage-icusin-remote
+
+cd /home/tw-camp-peking
+echo start deploy tw-camp-peking
+
+cd docker
+docker build -t peking .
+docker rm -f peking-instance
+docker run --name peking-instance -d -p 8000:8000 peking
 stage-icusin-remote
